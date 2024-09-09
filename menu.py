cbec38a3e2377662670c53488dbaafc18ba6d68d
eovelar <114450123+eovelar@users.noreply.github.com>
2024-09-09 19:43:12 -0300

    Menu (borrador)
    cambio de casteo de int a string para evitar errores en el input, inclusión de todos los print de opciones en una lista

--- menu.py
+++ menu.py
@@ -11,39 +11,38 @@ def limpiar_pantalla():
 
 def menu():
     opcion_seleccionada = 0
-    while opcion_seleccionada != 7:
+    while opcion_seleccionada != "7":
         limpiar_pantalla()  # Limpiar la pantalla antes de mostrar el menú
-        print("Menú de Gestión de Inventarios")
-        print("1. Añadir producto")
-        print("2. Editar producto")
-        print("3. Eliminar producto")
-        print("4. Ver inventario actual")
-        print("5. Buscar producto por ID")
-        print("6. Generar informe de inventario")
-        print("7. Salir")
-        opcion_seleccionada = int(input("Elija una opción: "))
+        opciones = ["Menú de Gestión de Inventarios", "1. Añadir producto",
+                    "2. Editar producto", "3. Eliminar producto",
+                    "4. Ver inventario actual", "5. Buscar producto por ID",
+                    "6. Generar informe de inventario", "7. Salir"]
+        for op in opciones:
+            print(op)        
+        opcion_seleccionada = input("Elija una opción: ")
 
         match opcion_seleccionada:
-            case 1:
+            case "1":
                 # Añadir un producto
                 pass
-            case 2:
+            case "2":
                 # Editar un producto
                 pass
-            case 3:
+            case "3":
                 # Eliminar un producto
                 pass
-            case 4:
+            case "4":
                 # Ver el inventario actual
                 pass
-            case 5:
+            case "5":
                 # Buscar un producto por ID
                 pass
-            case 6:
+            case "6":
                 # Generar informe
                 pass
-            case 7:
+            case "7":
                 print("Saliendo del programa...")
+                break
             case _:
                 print("Opción no válida, por favor intente nuevamente.")
 
