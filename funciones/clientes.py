3798801066dce01a9863fb26c76df650e922661f
Mora Ivanec <moraivanec2@gmail.com>
2024-10-25 12:07:11 -0300

    Actualización de funciones de productos y clientes.

--- funciones/clientes.py
+++ funciones/clientes.py
@@ -1,4 +1,4 @@
-def agregar_cliente(clientes: List[Dict[str, Any]]) -> None:
+def agregar_cliente(clientes: List[Dict[str, str]]) -> None:
     """
     Contrato:
     - Agrega un nuevo cliente a la lista de clientes.
@@ -9,9 +9,36 @@ def agregar_cliente(clientes: List[Dict[str, Any]]) -> None:
     Postcondiciones:
     - Agrega un nuevo cliente a la lista de "clientes"..
     """
-    print("Lo sentimos, esta función no está implementada. Intente más tarde.")
+    try:
+        assert nombre_cliente != "", "El nombre no puede estar vacío."
+        assert dni != "", "El DNI no puede estar vacío."
+        assert correo != "", "El correo no puede estar vacío."
+        
+        if not re.match(r"^[A-Za-z\s]+$", nombre_cliente):
+            print("El nombre solo puede tener letras y espacios.")
+            return
+            
+        if not re.match(r"^\d{8}$", dni):
+            print("El DNI debe tener exactamente 8 dígitos.")
+            return
+            
+        if not re.match(r"^[\w\.-]+@[\w\.-]+\.\w+$", correo):
+            print("El correo no es válido.")
+            return
+        
+        nuevo_cliente = {
+            "nombre": nombre_cliente,
+            "dni": dni,
+            "correo": correo
+        }
+        
+        clientes.append(nuevo_cliente)
+        print("¡Cliente agregado existosamente!")
 
-
+    except ValueError:
+        print("Error!")
+            
+   
 def editar_cliente(clientes: List[Dict[str, Any]]) -> None:
     """
     Contrato:
@@ -23,8 +50,43 @@ def editar_cliente(clientes: List[Dict[str, Any]]) -> None:
     Postcondiciones:
     - El cliente seleccionado es editado con los nuevos valores proporcionados.
     """
-    print("Lo sentimos, esta función no está implementada. Intente más tarde.")
-
+    try:
+        assert dni != "", "El DNI del cliente a editar no puede estar vacío."
+        
+        # Busco al cliente en la lista y lo edito si se encuentra
+        cliente_encontrado = False
+        for cliente in clientes:
+            if cliente["dni"] == dni:
+                cliente_encontrado = True
+                
+        assert cliente_encontrado, "Cliente no encontrado."
+                
+                assert nombre_cliente_editado != "", "El nuevo nombre no puede estar vacío."
+                assert dni_editado != "", "El nuevo DNI no puede estar vacío."
+                assert correo_editado != "", "El nuevo correo no puede estar vacío."
+                 
+                if not re.match(r"^[A-Za-z\s]+$", nombre_cliente_editado):
+                    print("El nuevo nombre solo puede tener letras y espacios.")
+                    return
+                    
+                if not re.match(r"^\d{8}$", dni_editado):
+                    print("El nuevo DNI debe tener exactamente 8 dígitos.")
+                    return
+            
+                if not re.match(r"^[\w\.-]+@[\w\.-]+\.\w+$", correo_editado):
+                    print("El nuevo correo no es válido.")
+                    return
+                    
+                # Actualizo los datps
+                cliente["nombre"] = nombre_cliente_editado
+                cliente["dni"] = dni_editado
+                cliente["correo"] = correo_editado
+                print("¡Cliente editado exitosamente!")
+                break
+                
+    except ValueError:
+        print("Error!")
+                    
 
 def eliminar_cliente(clientes: List[Dict[str, Any]]) -> None:
      """
@@ -37,8 +99,22 @@ def eliminar_cliente(clientes: List[Dict[str, Any]]) -> None:
     Postcondiciones:
     - El cliente seleccionado es eliminado de la lista de clientes.
     """
-    print("Lo sentimos, esta función no está implementada. Intente más tarde.")
-
+    try:
+        assert dni != "", "El DNI del cliente a editar no puede estar vacío."
+        
+        cliente_encontrado = False
+        for cliente in clientes:
+            if cliente["dni"] == dni:
+                cliente_encontrado = True
+                clientes.remove(cliente)  # Elimino el cliente de la lista
+                print("¡Cliente eliminado exitosamente!")
+                return
+            
+        assert cliente_encontrado, "Cliente no encontrado."
+        
+    except ValueError:
+        print("Error!"):
+        
 
 def consultar_lista_clientes(clientes: List[Dict[str, Any]]) -> None:
     """
--- funciones/productos.py
+++ funciones/productos.py
@@ -11,8 +11,42 @@ def cargar_producto(productos: List[Dict[str, Any]], stock: Dict[str, int]) -> N
     - Los productos son cargados en la lista "productos".
     - El stock es actualizado adecuadamente.
     """
-    print("Lo sentimos, esta función no está implementada. Intente más tarde.")
+    try:
+        assert id_producto != "", "El ID del producto no puede estar vacío."
+        assert nombre_producto != "", "El nombre del producto no puede estar vacío."
+        assert precio != "", "El precio del producto no puede estar vacío."
+        
+        if not re.match(r"^\d+$", id_producto):
+            print("El ID sólo puede tener números.")
+            return
 
+        if not re.match(r"^[A-Za-z\s]+$", nombre_producto):
+            print("El nombre solo puede tener letras y espacios.")
+            return
+            
+        if not re.match(r"^(0|[1-9]\d*)(\.\d{1,2})?$", precio):
+            print("El precio tiene que ser un número positivo que incluya hasta dos decimales.")
+            return
+            
+        if precio <= 0:
+            print("El precio tiene que ser un número positivo.")
+            return
+        
+        nuevo_producto = {
+            "id": id_producto,
+            "nombre": nombre_producto,
+            "precio": precio
+        }
+        
+        productos.append(nuevo_producto)
+        
+        # Inicio el stock en 0 si no existe
+        stock[id] = stock.get(id, 0)
+        print("¡Producto cargado exitosamente!")
+        
+    except ValueError:
+        print("Error!")
+        
 
 def editar_producto(productos: List[Dict[str, Any]], stock: Dict[str, int]) -> None:
     """
@@ -27,8 +61,50 @@ def editar_producto(productos: List[Dict[str, Any]], stock: Dict[str, int]) -> N
     - El producto seleccionado es editado con los nuevos valores proporcionados.
     - El stock del producto también puede ser actualizado si es necesario.
     """
-    print("Lo sentimos, esta función no está implementada. Intente más tarde.")
-
+    try:
+        assert id != "", "El ID del producto no puede estar vacío."
+        
+        producto_encontrado = False
+        for producto in productos:
+            if producto["id"] == id_producto:
+                producto_encontrado = True
+                
+        assert producto_encontrado, "Producto no encontrado"
+                
+                assert id_producto_editado != "", "El nuevo ID del producto no puede estar vacío."
+                assert nombre_producto_editado != "", "El nuevo nombre del producto no puede estar vacío."
+                assert precio_editado != "", "El nuevo precio del producto no puede estar vacío."
+                
+                if not re.match(r"^\d+$", id_producto_editado):
+                    print("El nuevo ID sólo puede tener números.")
+                    return
+                
+                if not re.match(r"^[A-Za-z\s]+$", nombre_producto_editado):
+                    print("El nuevo nombre solo puede tener letras y espacios.")
+                    return
+                
+                if not re.match(r"^(0|[1-9]\d*)(\.\d{1,2})?$", precio_editado):
+                    print("El nuevo precio tiene que ser un número positivo que incluya hasta dos decimales.")
+                    return
+                
+                if precio <= 0:
+                    print("El precio tiene que ser un número positivo.")
+                    return
+                
+                # Actualizo el stock solo si el ID del producto se cambia
+                if id_producto != id_producto_editado:
+                    stock[id_producto_editado] = stock.pop(id_producto, 0) # Muevo el stock del ID anterior al nuevo
+                
+                producto["id"] = id_producto_editado
+                producto["nombre"] = nombre_producto_editado
+                producto["precio"] = precio_editado
+                
+                print("¡Producto editado exitosamente!")
+                break
+            
+    except ValueError:
+        print("Error!")
+                
 
 def eliminar_producto(productos: List[Dict[str, Any]], stock: Dict[str, int]) -> None:
     """
@@ -43,8 +119,21 @@ def eliminar_producto(productos: List[Dict[str, Any]], stock: Dict[str, int]) ->
     - El producto seleccionado es eliminado de la lista de productos.
     - El stock del producto también es eliminado.
     """
-    print("Lo sentimos, esta función no está implementada. Intente más tarde.")
-
+    try:
+        assert id_producto != "", "El ID del producto no puede estar vacío."
+        
+        producto_encontrado = False
+        for producto in productos:
+            if producto["id"] == id_producto:
+                producto_encontrado = True
+                productos.remove(producto)  # Eliminar el producto de la lista
+                print("¡Producto eliminado exitosamente!")
+                break
+        
+        assert producto_encontrado, "Producto no encontrado."
+    
+    except ValueError:
+        print("Error!")
 
 def consultar_lista_productos() -> None:
   """
